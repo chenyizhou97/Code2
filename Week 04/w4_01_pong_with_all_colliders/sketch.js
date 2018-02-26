0262fae4b67222bca4287f9b0b11a502b1323144
chenyizhou97 <chenyizhou97@users.noreply.github.com>
2018-02-26 15:00:59 -0500

    debug finish

--- Week 04/w4_01_pong_with_all_colliders/sketch.js
+++ Week 04/w4_01_pong_with_all_colliders/sketch.js
@@ -16,13 +16,14 @@ var sceneState = {
   LEVEL1: 1,
   LEVEL2: 2,
   LEVEL3: 3,
+  LEVEL4: 4,
   LEVEL5: 5,
   LEVEL6: 6,
   LEVEL7: 7,
   LEVEL8: 8,
   LEVEL9: 9,
   LEVEL10: 10,
-  WIN: 11,
+  WIN: 10,
 };
 var keyOn = false;
 var currentState = sceneState.INTRO;
@@ -301,7 +302,8 @@ function drawScene(whichScene) {
     case sceneState.LEVEL10:
        colliders.push(new Cat());
        break;
-    case sceneState.WIN:
+    case sceneState.WIN
+      background(0);
       drawField();
       if(p1Score>p2Score){
         fill(255);
